c65e5ee704a8c0504dbdc130fe62a2bfb136257c
Ashish <g.ash2010@gmail.com>
2024-02-13 15:32:54 +0530

    comit from cloned updated

--- Baseclasss.py
+++ Baseclasss.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWaitttt
 
 from tests import homepage
 
